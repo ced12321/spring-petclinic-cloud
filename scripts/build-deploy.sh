a47d0c29ec27b5ba0da2ec542167b3100e99ce8c
CDM <CedricMaubach@web.de>
2025-06-28 16:26:57 +0200

    add clean packe to build script

--- scripts/build-deploy.sh
+++ scripts/build-deploy.sh
@@ -7,6 +7,14 @@ set -o pipefail
 export REPOSITORY_PREFIX=cedricmaubach
 echo "REPOSITORY_PREFIX auf '$REPOSITORY_PREFIX' gesetzt."
 
+echo "installing maven dependencies..."
+./mvnw clean install -DskipTests
+sleep 2
+
+echo "packaging mit Maven..."
+./mvnw package -Pk8s
+sleep 2
+
 # 2. Erstelle Container-Images
 echo "Erstelle Container-Images mit Maven..."
 ./mvnw spring-boot:build-image -Pk8s -DREPOSITORY_PREFIX=${REPOSITORY_PREFIX}
